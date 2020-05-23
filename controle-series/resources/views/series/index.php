30b124ddd2011e66fce2a2a998bf2614c604f12f
farleybsd <farley.t.i@hotmail.com>
2020-05-23 20:13:03 -0300

    Estilizando a View

--- controle-series/resources/views/series/index.php
+++ controle-series/resources/views/series/index.php
@@ -4,15 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Controle de Series</title>
 </head>
 
 <body>
-    <ul>
-        <?php foreach ($series as $serie) : ?>
-            <li><?= $serie; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="container">
+
+        <div class="jumbotron">
+            <h1>Séries</h1>
+        </div>
+
+        <a href="#" class="btn btn-dark mb-2">Adicionar</a>
+
+        <ul class="list-group">
+            <?php foreach ($series as $serie) : ?>
+                <li class="list-group-item"><?= $serie; ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+    </div>
 </body>
 
 </html>
