39a5ef6486bc19cfb641205d676554f4129b5885
farleybsd <farley.t.i@hotmail.com>
2020-05-23 19:49:46 -0300

    Aprendendo a lidar com o Request

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -3,11 +3,17 @@
 namespace App\http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\PHP;
 
 class SeriesController extends Controller
 {
-    public  function listarseries()
+    public  function index(Request $request)
     {
+        //echo $request->url() . PHP_EOL;
+        //echo $request->query('parametro'); //http://127.0.0.1:8000/series?parametro=valor
+        //var_dump($request->query()); //http://127.0.0.1:8000/series?parametro=valor?nome=%27Farley%27?idade=28 traz todos parametros como array
+        //exit();
 
         $series = [
             'Greys\'s Anatomy',
--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', 'SeriesController@listarseries');
+Route::get('/series', 'SeriesController@index');
